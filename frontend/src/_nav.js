da21bca4aba6ecf2388fbd95bc24add20748743b
ivan horomanski <ivan.horomanski@odifis.com>
2019-10-19 20:42:02 +0200

    adding frontend

---
+++ frontend/src/_nav.js
@@ -0,0 +1,77 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info',
+        text: 'NEW',
+      },
+    },
+   
+    {
+      name: 'Buttons',
+      url: '/buttons',
+      icon: 'icon-cursor',
+      children: [
+        {
+          name: 'Buttons',
+          url: '/buttons/buttons',
+          icon: 'icon-cursor',
+        },
+        {
+          name: 'Button dropdowns',
+          url: '/buttons/button-dropdowns',
+          icon: 'icon-cursor',
+        },
+        {
+          name: 'Button groups',
+          url: '/buttons/button-groups',
+          icon: 'icon-cursor',
+        },
+        {
+          name: 'Brand Buttons',
+          url: '/buttons/brand-buttons',
+          icon: 'icon-cursor',
+        },
+      ],
+    },
+  
+    {
+      name: 'Notifications',
+      url: '/notifications',
+      icon: 'icon-bell',
+      children: [
+        {
+          name: 'Alerts',
+          url: '/notifications/alerts',
+          icon: 'icon-bell',
+        },
+        {
+          name: 'Badges',
+          url: '/notifications/badges',
+          icon: 'icon-bell',
+        },
+        {
+          name: 'Modals',
+          url: '/notifications/modals',
+          icon: 'icon-bell',
+        },
+      ],
+    },
+    {
+      name: 'Widgets',
+      url: '/widgets',
+      icon: 'icon-calculator',
+      badge: {
+        variant: 'info',
+        text: 'NEW',
+      },
+    },
+    {
+      divider: true,
+    },
+
+  ],
+};
